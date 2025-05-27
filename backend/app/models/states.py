e581a2afd7d8a4cb1c47b3f9a83db252cd64d490
Rahul A <rahul72372003@gmail.com>
2025-05-27 20:51:29 +0530

    chorma db and cache

--- backend/app/models/states.py
+++
@@ -1,8 +0,0 @@
-from typing import TypedDict, List, Dict
-from langchain_core.documents import Document
-
-class QueryState(TypedDict):
-    query: str
-    results: List[Document]
-    answers: List[Dict]
-    themes: Dict[str, List[str]]
