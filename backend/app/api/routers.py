9d7a925d89d58f5358b218d8f58f99e95336125d
Rahul A <rahul72372003@gmail.com>
2025-05-28 05:40:54 +0530

    new_update

---
+++ backend/app/api/routers.py
@@ -0,0 +1,18 @@
+from  fastapi import  FastAPI
+from .endpoints.document_api import MainApi
+from ..models.api_models import *
+app = FastAPI()
+
+query_router=MainApi(prefix="/api/query",model=QueryAPI,tags="query")
+query_router.add_routes()
+
+document_router=MainApi(prefix="/api/document",model=DocumentAPI,tags="document")
+document_router.add_routes()
+
+theme_router=MainApi(prefix="/api/theme",model=ThemeAPI,tags="theme")
+theme_router.add_routes()
+
+
+app.include_router(query_router.router)
+app.include_router(document_router.router)
+app.include_router(theme_router.router)
